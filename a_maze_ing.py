a1efeb8b24226fc7b833d9b0564ca20c051461b7
douaeabrouri <abrouridoae@gmail.com>
2026-03-13 00:57:50 +0000

    ...

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import sys
 
+
+
 def parsing(filepath: str) -> None:
     raw: dict = {}
     requierd_keys: list = ["WIDTH", "HEIGHT", "ENTRY", "EXIT", "OUTPUT_FILE", "PERFECT"]
+    """step 1 : open the file"""
     try:
-        lines: list = {}
+        lines: list = []
         with open("config.txt", "r") as file:
             lines = file.readlines()
     except FileNotFoundError as e:
@@ -13,7 +16,7 @@ def parsing(filepath: str) -> None:
         return None
     except OSError as e:
         print(f"Error reading config file: {e}")
-        
+    """step 2 : parse lines"""
     for line_num, line in lines[:]:
         line = line.strip()
         if "=" not in line:
@@ -22,15 +25,18 @@ def parsing(filepath: str) -> None:
         key, _, value = line.partition("=")
         key = key.strip()
         value = value.strip()
- 
         if not key or not value:
             print(f"Error: line {line_num} has an empty key or value.")
             return None
- 
         raw[key] = value
- 
-    # --- Step 3: Check all required keys are present ---
+    """step 3: check requierd keys"""
     for key in requierd_keys:
         if key not in raw:
             print(f"Error: missing required key '{key}' in config file.")
             return None
+    """step 4: valiate each value  """
+    for key, value in raw[:]:
+        if value == "WIDTH" and key < 0:
+            print("Error the WIDTH must be positive number!!!")
+            return None
+        if value == 
